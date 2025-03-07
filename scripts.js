e13e0259d37358bf940c8e2f9cdc83c762a4fad6
LucasFriedrich10 <lucasfriedrich10@hotmail.com>
2025-03-06 23:00:01 -0300

    Removendo item da lista

--- scripts.js
+++ scripts.js
@@ -147,3 +147,18 @@ function updateTotals(){
         console.log(error)
     }
 }
+
+//Evento que captura clique nos itens da lista
+expenseList.addEventListener("click", function(event){
+    //Verificar se o elemento clicado é o icone de remover
+
+    if(event.target.classList.contains("remove-icon")){
+        //Obtem a LI pai do elemento clicado
+        const item = event.target.closest(".expense")
+
+        //Remove o item da lista
+        item.remove()
+    }
+    //Atualiza os totais
+    updateTotals()
+})
